8642ddc6ad1d2f5465c4f5ca2f1328251ff9641b
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2021-06-05 20:43:14 -0300

    Clases Carrito e Item
    Creé la clase de carrito e items de carrito, para trabajar a la par

---
+++ SBKEY/Dominio/Carrito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Carrito
+    { 
+            public List<ItemCarrito> Items { get; set; }
+       
+    }
+
+
+    public class ItemCarrito
+    {
+        public Articulo _articulo { get; set; }
+        public int cantidad { get; set; }
+
+        public double precioSubTotal { get; set; }
+    }
+
+}
